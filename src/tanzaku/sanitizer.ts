34d1b382a543ff50e9b1285e76531dfcd1fec5d6
mizphses <fuminori@mizphs.es>
2024-06-25 15:52:43 +0900

    NGワード処理

--- src/tanzaku/sanitizer.ts
+++ src/tanzaku/sanitizer.ts
@@ -1,9 +1,20 @@
 import * as sanitizeHtml from 'sanitize-html';
+import { ngwords } from './ngwords';
 
 export const sanitizer = (input: string, max: number) => {
   const clean = sanitizeHtml(input, {
     allowedTags: [],
     allowedAttributes: {},
   });
-  return clean.slice(0, max);
+  if (ngwords.includes(clean)) {
+    return "";
+  }
+  return hanZen(clean).slice(0, max);
+}
+
+const hanZen = (str: string) => {
+  // ローマ字を全部全角に
+  return str.replace(/[A-Za-z0-9]/g, (s) => {
+    return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
+  });
 }
